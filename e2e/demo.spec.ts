477897e846bb41f0501c78a5675f597572087a3c
Raúl Gutiérrez <raul.gutierrez4000@gmail.com>
2025-09-02 21:09:31 -0400

    Separación de casos

--- e2e/demo.spec.ts
+++ e2e/demo.spec.ts
@@ -18,7 +18,7 @@ test.beforeEach(async ({ page, context }) => {
 
 //test.describe(`Tests Demo: `, () => {
 
-test('Flujo DEMO', async ({ page }) => {
+test('Flujo DEMO', async () => {
   //inicio de sesion
   await functionDemo.login();
 
@@ -43,6 +43,41 @@ test('Flujo DEMO', async ({ page }) => {
 
   // navegacion al modulo de Ventas Crear Pedido de Venta
   await functionDemo.createSalesOrder(nombreCliente);
+});
+
+
+test.describe('Functionality Tests', () => {
+const nombreProducto = "Producto-" + randomString(6);
+const nombreCliente = "Cliente-" + randomString(6);
+
+  test('Crear Producto', async () => {
+    await functionDemo.login();
+    const expectResultProduct = await functionDemo.createProduct(nombreProducto);
+    expect(expectResultProduct).toBe(true);
+  });
+
+  test('Crear Cliente', async () => {
+    await functionDemo.login();
+    const expectResultCustomer = await functionDemo.createCustomer(nombreCliente);
+    expect(expectResultCustomer).toBe(true);
+  });
+
+  test('Crear Cotizacion', async () => {
+    await functionDemo.login();
+    const expectResultCotization = await functionDemo.createCotization(nombreCliente, nombreProducto);
+    expect(expectResultCotization).toBe(true);
+  });
+
+  test('Cambiar Status Cotizacion', async () => {
+    await functionDemo.login();
+    const expectResultChangeStatus = await functionDemo.changeStatusCotization(nombreCliente);
+    expect(expectResultChangeStatus).toBe(true);
+  });
+
+  test('Crear Pedido de Venta', async () => {
+    await functionDemo.login();
+    await functionDemo.createSalesOrder(nombreCliente);
+  });
 });
 
 test.afterEach(async () => {await mainPage.close();});
